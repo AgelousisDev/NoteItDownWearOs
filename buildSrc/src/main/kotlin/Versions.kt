5f6c7cd5d93947e4019c60e810c179fa981808c6
Evangelos Angelousis <vage.solutions@gmail.com>
2023-06-30 14:26:32 +0300

    Dependency Updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,12 +1,12 @@
 object Versions {
     const val gradlePlugin = "8.0.2"
-    const val kotlinVersion = "1.8.21"
-    const val composeVersion = "1.5.0-beta01"
-    const val composeActivityVersion = "1.7.0-alpha03"
-    const val coreKtxVersion = "1.10.0-alpha02"
+    const val kotlinVersion = "1.8.22"
+    const val composeVersion = "1.6.0-alpha01"
+    const val composeActivityVersion = "1.8.0-alpha06"
+    const val coreKtxVersion = "1.12.0-alpha05"
     const val googlePlayServicesWearableVersion = "18.0.0"
     const val percentLayoutVersion = "1.0.0"
-    const val wearComposeVersion = "1.2.0-beta01"
+    const val wearComposeVersion = "1.3.0-alpha01"
     const val wearInputVersion = "1.2.0-alpha02"
     const val liveDataViewModelVersion = "2.6.1"
     const val dataStorePreferenceVersion = "1.0.0"
@@ -15,8 +15,8 @@ object Versions {
     const val glanceWearTileVersion = "1.0.0-alpha05"
     const val retrofit2ConverterGsonVersion = "2.9.0"
     const val http3LoggingInterceptorVersion = "5.0.0-alpha.8"
-    const val kotlinCompilerExtensionVersion = "1.4.7"
-    const val glanceVersion = "1.0.0-alpha05"
+    const val kotlinCompilerExtensionVersion = "1.4.8"
+    const val glanceVersion = "1.0.0-beta01"
     const val glanceAppWidgetVersion = "1.0.0-beta01"
 }
 object ConfigData {
