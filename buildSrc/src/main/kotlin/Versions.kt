0374026f5b250ac4be44674dae55563c19560d72
Evangelos Angelousis <vage.solutions@gmail.com>
2023-10-27 14:42:50 +0300

    Dependency updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,17 +1,17 @@
 object Versions {
     const val gradlePlugin = "8.1.2"
     const val kotlinVersion = "1.9.10"
-    const val composeVersion = "1.6.0-alpha07"
+    const val composeVersion = "1.6.0-alpha08"
     const val composeActivityVersion = "1.8.0-rc01"
     const val coreKtxVersion = "1.12.0-rc01"
     const val googlePlayServicesWearableVersion = "18.0.0"
     const val percentLayoutVersion = "1.0.0"
-    const val wearComposeVersion = "1.3.0-alpha07"
+    const val wearComposeVersion = "1.3.0-alpha08"
     const val wearInputVersion = "1.2.0-alpha02"
-    const val liveDataViewModelVersion = "2.7.0-alpha02"
+    const val liveDataViewModelVersion = "2.7.0-alpha03"
     const val dataStorePreferenceVersion = "1.0.0"
     const val constraintLayoutComposeVersion = "1.1.0-alpha13"
-    const val lifecycleRuntimeKtxVersion = "2.7.0-alpha02"
+    const val lifecycleRuntimeKtxVersion = "2.7.0-alpha03"
     const val glanceWearTileVersion = "1.0.0-alpha05"
     const val retrofit2ConverterGsonVersion = "2.9.0"
     const val http3LoggingInterceptorVersion = "5.0.0-alpha.8"
@@ -19,7 +19,7 @@ object Versions {
     const val glanceVersion = "1.0.0-rc01"
     const val glanceAppWidgetVersion = "1.0.0-beta01"
     const val githubBanesVersion = "0.49.0"
-    const val wearToolingPreviewDevicesVersion = "1.0.0-alpha01"
+    const val wearToolingPreviewDevicesVersion = "1.0.0-beta01"
 }
 object ConfigData {
     const val compileSdkVersion = 34
