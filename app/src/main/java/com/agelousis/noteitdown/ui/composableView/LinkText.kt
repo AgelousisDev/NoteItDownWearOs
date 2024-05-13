c413ddf54d5846faef4fb6bc5513a60117b1b27b
Evangelos Angelousis <vagelis_agelousis@outlook.com>
2024-05-13 14:47:29 +0300

    Dependency Updates

--- app/src/main/java/com/agelousis/noteitdown/ui/composableView/LinkText.kt
+++ app/src/main/java/com/agelousis/noteitdown/ui/composableView/LinkText.kt
@@ -1,23 +1,30 @@
 package com.agelousis.noteitdown.ui.composableView
 
-import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material3.MaterialTheme
+import androidx.wear.compose.material3.Text
+import com.agelousis.noteitdown.ui.extensions.whiteRoundedBackgroundModifier
+import com.agelousis.noteitdown.ui.theme.NoteItDownTheme
 
 data class LinkTextData(
     val text: String,
-    val tag: String? = null,
     val textColor: Color? = null,
     val fontWeight: FontWeight? = null,
     val fontSize: TextUnit? = null,
+    val tag: String? = null,
     val annotation: String? = null,
-    val onClick: ((str: AnnotatedString.Range<String>) -> Unit)? = null,
+    val onClick: ((url: String) -> Unit)? = null,
     val decoration: TextDecoration = TextDecoration.None
 )
 //Spannable
@@ -32,24 +39,28 @@ fun LinkText(
         data = linkTextData
     )
 
-    ClickableText(
-        text = annotatedString,
-        style = style,
-        onClick = { offset ->
-            onClickText?.invoke()
-            linkTextData.forEach { annotatedStringData ->
-                if (annotatedStringData.tag != null && annotatedStringData.annotation != null) {
-                    annotatedString.getStringAnnotations(
-                        tag = annotatedStringData.tag,
-                        start = offset,
-                        end = offset,
-                    ).firstOrNull()?.let {
-                        annotatedStringData.onClick?.invoke(it)
+    Text(
+        modifier = modifier
+            .clickable(
+                onClick = {
+                    onClickText?.invoke()
+                    linkTextData.forEachIndexed { index, linkTextData ->
+                        if (linkTextData.annotation != null) {
+                            linkTextData.onClick?.invoke(
+                                annotatedString.getStringAnnotations(
+                                    start = 0,
+                                    end = linkTextData.tag?.length
+                                        ?: return@forEachIndexed
+                                ).getOrNull(
+                                    index = index
+                                )?.tag ?: ""
+                            )
+                        }
                     }
                 }
-            }
-        },
-        modifier = modifier
+            ),
+        text = annotatedString,
+        style = style
     )
 }
 
@@ -58,10 +69,12 @@ private fun createAnnotatedString(
     data: List<LinkTextData>
 ) = buildAnnotatedString {
     data.forEach { linkTextData ->
-        if (linkTextData.tag != null && linkTextData.annotation != null) {
+        if (!linkTextData.tag.isNullOrEmpty()
+            && !linkTextData.annotation.isNullOrEmpty()
+        ) {
             pushStringAnnotation(
                 tag = linkTextData.tag,
-                annotation = linkTextData.annotation,
+                annotation = linkTextData.annotation
             )
             withStyle(
                 style = SpanStyle(
@@ -71,7 +84,9 @@ private fun createAnnotatedString(
                     fontSize = linkTextData.fontSize ?: TextUnit.Unspecified
                 )
             ) {
-                append(linkTextData.text)
+                append(
+                    text = linkTextData.text
+                )
             }
             pop()
         } else {
@@ -83,8 +98,42 @@ private fun createAnnotatedString(
                     fontSize = linkTextData.fontSize ?: TextUnit.Unspecified
                 )
             ) {
-                append(linkTextData.text)
+                append(
+                    text = linkTextData.text
+                )
             }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LinkTextPreview() {
+    NoteItDownTheme {
+        Box(
+            modifier = Modifier
+                .padding(
+                    top = 50.dp
+                )
+                .whiteRoundedBackgroundModifier
+        ) {
+            LinkText(
+                modifier = Modifier
+                    .padding(
+                        all = 16.dp
+                    ),
+                linkTextData = listOf(
+                    LinkTextData(
+                        text = "Url: "
+                    ),
+                    LinkTextData(
+                        text = "GOOGLE",
+                        tag = "google",
+                        annotation = "https://google.com"
+                    )
+                ),
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
