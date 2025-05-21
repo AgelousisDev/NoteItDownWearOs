e58ef857d9a184dfbac3a63f82ae0bdb07481688
Evangelos Angelousis <vagelis_agelousis@outlook.com>
2025-05-21 10:43:55 +0300

    Dependency Updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,17 +1,17 @@
 object Versions {
-    const val GRADLE_PLUGIN = "8.8.0"
-    const val KOTLIN_VERSION = "2.1.20-Beta1"
-    const val COMPOSE_VERSION = "1.7.3"
-    const val COMPOSE_ACTIVITY_VERSION = "1.9.0"
-    const val CORE_KTX_VERSION = "1.13.1"
-    const val WEAR_COMPOSE_VERSION = "1.4.0"
+    const val GRADLE_PLUGIN = "8.9.2"
+    const val KOTLIN_VERSION = "2.2.0-Beta2"
+    const val COMPOSE_VERSION = "1.9.0-alpha03"
+    const val COMPOSE_ACTIVITY_VERSION = "1.12.0-alpha01"
+    const val CORE_KTX_VERSION = "1.16.0"
+    const val WEAR_COMPOSE_VERSION = "1.5.0-alpha13"
     const val WEAR_INPUT_VERSION = "1.2.0-alpha02"
-    const val LIVE_DATA_VIEW_MODEL_VERSION = "2.8.5"
+    const val LIVE_DATA_VIEW_MODEL_VERSION = "2.9.0"
     const val DATA_STORE_PREFERENCE_VERSION = "1.1.1"
-    const val CONSTRAINT_LAYOUT_COMPOSE_VERSION = "1.0.1"
-    const val LIFECYCLE_RUNTIME_KTX_VERSION = "2.8.5"
+    const val CONSTRAINT_LAYOUT_COMPOSE_VERSION = "1.1.1"
+    const val LIFECYCLE_RUNTIME_KTX_VERSION = "2.9.0"
     const val GLANCE_WEAR_TILE_VERSION = "1.0.0-alpha05"
-    const val RETROFIT_CONVERTER_GSON_VERSION = "2.11.0"
+    const val RETROFIT_CONVERTER_GSON_VERSION = "3.0.0"
     const val HTTP_LOGGING_INTERCEPTOR_VERSION = "5.0.0-alpha.14"
 
     const val KOTLIN_COMPILER_EXTENSION_VERSION = "1.5.15"
@@ -20,12 +20,12 @@ object Versions {
     const val GITHUB_BANES_VERSION = "0.52.0"
     const val WEAR_TOOLING_PREVIEW_DEVICES_VERSION = "1.0.0"
     const val COIL_COMPOSE_VERSION = "2.7.0"
-    const val WEAR_COMPOSE_MATERIAL_YOU_VERSION = "1.0.0-alpha23"
+    const val WEAR_COMPOSE_MATERIAL_YOU_VERSION = "1.5.0-beta02"
 }
 object ConfigData {
-    const val COMPILE_SDK_VERSION = 34
+    const val COMPILE_SDK_VERSION = 36
     const val MIN_SDK_VERSION = 28
-    const val TARGET_SDK_VERSION = 34
+    const val TARGET_SDK_VERSION = 36
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
     const val PACKAGE_NAME = "com.agelousis.noteitdown"
